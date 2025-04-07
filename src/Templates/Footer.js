56e2f9112898db8e12a417d7c949cc80d2abe988
harshal-012 <131434364+harshal-012@users.noreply.github.com>
2025-04-07 11:14:04 +0530

    Update Footer.js

--- src/Templates/Footer.js
+++ src/Templates/Footer.js
@@ -1,7 +1,7 @@
 const navigation = [
   {
     name: "YouTube",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    href: "https://www.youtube.com/",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -14,7 +14,7 @@ const navigation = [
   },
   {
     name: "Facebook",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    href: "https://www.facebook.com/",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -27,7 +27,7 @@ const navigation = [
   },
   {
     name: "Instagram",
-    href: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    href: "https://www.instagram.com/accounts/login/?hl=en",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
@@ -39,7 +39,7 @@ const navigation = [
     ),
   },
   {
-    name: "https://www.youtube.com/channel/UCqgi3TTpWwO22hIxzPOLhWw",
+    name: "https://www.youtube.com/",
     href: "#",
     icon: (props) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
@@ -80,7 +80,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2023 Inovotek Coding Academy, Inc. All rights reserved.
+            &copy; 2025 Harshal Parmar, Inc. All rights reserved.
           </p>
         </div>
       </div>
